db315639b4bbfb49039e4ed2c023d6e881ae4097
Nishimwe Prince <31816352+nishimweprince@users.noreply.github.com>
2023-01-03 21:34:06 +0000

    Progress

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Amplify } from 'aws-amplify/';
 import awsconfig from './aws-exports';
 import { withAuthenticator } from '@aws-amplify/ui-react'
 import { Button } from './Button'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import IngaboCreateForm from './ui-components/IngaboCreateForm';
 import IngaboUpdateForm from './ui-components/IngaboUpdateForm';
 import NavBar from './NavBar';
@@ -21,49 +21,18 @@ function App() {
 
 
   return (
-    <Router>
-      <Helmet>
-                <meta charSet="utf-8" />
-                <title>Ingabo Syndicate Database</title>
-                <link rel="canonical" href="http://mysite.com/example" />
-            </Helmet>
-      <div className="App">
-        <div className="content">
-          <NavBar />
 
+    <>
+    
+    <NavBar />
 
-          <Routes>
-            <Route exact path="/">
-              <div className="min-h-screen bg-gray-100 text-gray-900">
-                <main className="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 pt-4">
-                  <div className=""></div>
-                  <div className="mt-4">
-                    <Table />
-                  </div>
-                </main>
-              </div>
-            </Route>
+    <Routes>
+      <Route path="/" element={<Table />} />
+      <Route path="/create" element={<IngaboCreateForm />} />
+    </Routes>
 
-            <div className="input-container">
+    </>
 
-              <Route path="/input">
-                <Input />
-              </Route>
-              
-              <Route path="/update">
-                <IngaboUpdateForm
-                  
-                onSuccess={() => {
-                  console.log("Data saved successfully");
-                }}
-              />
-            </Route>
-
-            </div>
-          </Routes>
-        </div>
-      </div>
-    </Router>
   );
 }
 
