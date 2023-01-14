62ccbe5bb7fc55e5c271b1eecca97e0469542ba4
Nishimwe Prince <31816352+nishimweprince@users.noreply.github.com>
2023-01-14 10:57:29 +0000

    Added twilio messaging functionality

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const twilio = require('twilio');
 const port = 5000;
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
@@ -14,22 +16,24 @@ console.log(accountSid, authToken, accountTelephone);
 // Create a new Twilio client
 const client = new twilio(accountSid, authToken);
 
-app.get("/:universalURL", (req, res) => {
-  res.send("404 URL NOT FOUND");
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("WELCOME TO OUR EXPRESS SERVER");
 });
 
 app.post('/messages', (req, res) => {
 
-  console.log('send-message is now working!')
     // Get the phone number and message text from the request body
-    const { phoneNumber, messageText } = req.body;
+    const { recipient, message } = req.body;
   
     // Send the message using the Twilio client
     client.messages
       .create({
-         body: messageText,
+         body: message,
          from: accountTelephone,
-         to: phoneNumber
+         to: recipient
        })
       .then(message => {
         res.send({
@@ -41,7 +45,7 @@ app.post('/messages', (req, res) => {
         console.log(err);
         res.send({
           status: 'error',
-          message: 'An error occurred while sending the message'
+          message: err.message
         });
       });
   });
