9090618239fca8c6197d1454e1ec57f04fdd66e4
Nishimwe Prince <princeelysee@gmail.com>
2023-01-19 06:51:22 +0000

    Deploying to heroku

--- src/Table.js
+++ src/Table.js
@@ -40,7 +40,7 @@ export function sendMessage(to, message){
   const endpoint = process.env.REACT_APP_ENDPOINT_URL;
   const localhost = "http://localhost";
   const ec2 = "http://44.209.248.214";
-  const port = "5000";
+  const port = "5000" || process.env.PORT;
   console.log(endpoint);
 
   const recipient = "+250" + to.slice(-9);
